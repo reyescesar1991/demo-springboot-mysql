5af19efcede2c8ced3d7506411eb34589b41cad2
reyescesar0711@gmail.com <reyescesar0711@gmail.com>
2026-01-07 15:37:00 -0400

    Limpiar rama para el uso de api rest

--- src/main/java/com/example/demo/ProyectoPruebaApplication.java
+++ src/main/java/com/example/demo/ProyectoPruebaApplication.java
@@ -18,16 +18,6 @@ public class ProyectoPruebaApplication {
 
 		var ctx = app.run(args);
 
-		MyFirstService myFirstService = ctx.getBean(MyFirstService.class);
-
-		System.out.println(myFirstService.tellAStory());
-		// System.out.println(myFirstService.getJavaVersion());
-		// System.out.println(myFirstService.getOsName());
-		// System.out.println(myFirstService.getOsVersion());
-		// System.out.println(myFirstService.readProps());
-		System.out.println(myFirstService.readCustomEnvProps());
-		// System.out.println(myFirstService.readCustomProps2());
-		// System.out.println("Custom property as int: " + myFirstService.readCustomPropsInt());
 	}
 
 }
