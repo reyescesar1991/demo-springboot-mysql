4558717acf8a931d4476a0ef66191d68ed46614d
reyescesar0711@gmail.com <reyescesar0711@gmail.com>
2026-01-15 16:21:57 -0400

    Quitar un import que no se usaba

--- src/test/java/com/example/demo/student/StudentMapperTest.java
+++ src/test/java/com/example/demo/student/StudentMapperTest.java
@@ -2,7 +2,6 @@ package com.example.demo.student;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import org.junit.jupiter.api.AfterAll;
